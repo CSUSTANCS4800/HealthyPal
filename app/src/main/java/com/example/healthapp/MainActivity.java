a1fcbf5f181b24f2b7049ed103db85b75ea550ed
Samir Espinoza <samirespinoza50@gmail.com>
2021-05-17 00:08:07 -0700

    got database to save dat

--- app/src/main/java/com/example/healthapp/MainActivity.java
+++ app/src/main/java/com/example/healthapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.healthapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +7,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.parse.LogInCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
-import com.parse.ParseInstallation;
 import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     EditText usernameEditText;
     EditText passwordEditText;
     Button loginButton;
-    Button signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,27 +61,27 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ParseInstallation.getCurrentInstallation().saveInBackground();
-        usernameEditText=findViewById(R.id.et_username);
-        passwordEditText=findViewById(R.id.et_password);
-        loginButton=findViewById(R.id.bt_submit);
-        signUpButton=findViewById(R.id.bt_signUpButton);
+        usernameEditText = findViewById(R.id.et_username);
+        passwordEditText = findViewById(R.id.et_password);
+        loginButton=findViewById(R.id.bt_login);
+
+
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!usernameEditText.getText().toString().isEmpty() && !passwordEditText.getText().toString().isEmpty()){
+                if (!usernameEditText.getText().toString().isEmpty() && !passwordEditText.getText().toString().isEmpty()) {
                     ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
                         @Override
                         public void done(ParseUser user, ParseException e) {
-                            if (user!=null){
-                                Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_LONG).show();
+                            if (user != null) {
+                                Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_LONG).show();
                                 startUserActivity();
-                            }else {
-                                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
                             }
                         }
                     });
@@ -93,34 +90,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!usernameEditText.getText().toString().isEmpty() && !passwordEditText.getText().toString().isEmpty()){
-                    ParseUser user = new ParseUser();
-                    user.setUsername(usernameEditText.getText().toString());
-                    user.setPassword(passwordEditText.getText().toString());
-                    user.signUpInBackground(new SignUpCallback() {
-                        @Override
-                        public void done(ParseException e) {
-                            if (e==null){
-                                Toast.makeText(getApplicationContext(),"Sign Up Successful",Toast.LENGTH_LONG).show();
-                                startUserActivity();
-                            }else {
-                                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-                }
 
-
-            }
-        });
 
     }
 
     private void startUserActivity() {
-        Intent intent = new Intent(MainActivity.this,UserActivity.class);
+       Intent intent = new Intent(MainActivity.this, UserActivity.class);
+        startActivity(intent);
+    }
+
+    public void signup(View view) {
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void forgotPassword(View view) {
+        Intent intent = new Intent(MainActivity.this, ResetPasswordActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/healthapp/SignUpActivity.java
@@ -0,0 +1,69 @@
+package com.example.healthapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText edName, edEmail, edPassword, edConfirmPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edName = findViewById(R.id.edName);
+        edEmail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPassword);
+        edConfirmPassword = findViewById(R.id.edConfirmPassword);
+    }
+
+    public void signup(View view) {
+        if( TextUtils.isEmpty(edName.getText())){
+            edName.setError( "Name is required!" );
+        }else if( TextUtils.isEmpty(edEmail.getText())){
+            edEmail.setError( "Email is required!" );
+        }else if( TextUtils.isEmpty(edPassword.getText())){
+            edPassword.setError( "Password is required!" );
+        }else if( TextUtils.isEmpty(edConfirmPassword.getText())){
+            edConfirmPassword.setError( "Confirm password is required!" );
+        }else if(!edPassword.getText().toString().equals(edConfirmPassword.getText().toString())){
+            Toast.makeText(SignUpActivity.this, "Passwords are not the same!", Toast.LENGTH_LONG).show();
+        }else{
+
+            final ProgressDialog progress = new ProgressDialog(this);
+            progress.setMessage("Loading ...");
+            progress.show();
+            ParseUser user = new ParseUser();
+            user.setUsername(edEmail.getText().toString().trim());
+            user.setEmail(edEmail.getText().toString().trim());
+            user.setPassword(edPassword.getText().toString());
+            user.put("name", edName.getText().toString().trim());
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    progress.dismiss();
+                    if (e == null) {
+                        Toast.makeText(SignUpActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        ParseUser.logOut();
+                        Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+}
